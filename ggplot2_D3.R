195663278824bc57ad64200e284e2ade5933e43f
mazed <mazedur@gmail.com>
2021-08-01 23:58:50 +0600

    ggplot2 install & datasets

---
+++ ggplot2_D3.R
@@ -0,0 +1,33 @@
+data(USJudgeRatings)
+str(USJudgeRatings)
+boxplot(USJudgeRatings$RTEN)
+boxplot(USJudgeRatings$RTEN, horizontal = TRUE)
+boxplot(USJudgeRatings$RTEN, horizontal = TRUE, xlab = "Lawyer Ratings")
+boxplot(USJudgeRatings$RTEN, horizontal = TRUE, notch =  TRUE, xlab = "Lawyer Ratings")
+boxplot(USJudgeRatings$RTEN, horizontal = TRUE, notch =  TRUE, xlab = "Lawyer Ratings", ylim = c(0,10)
+boxplot(USJudgeRatings$RTEN, horizontal = TRUE, notch =  TRUE, xlab = "Lawyer Ratings", ylim = c(0,10))
+boxplot(USJudgeRatings$RTEN, horizontal = TRUE, notch =  TRUE, xlab = "Lawyer Ratings", ylim = c(0,10),col = "slategory")
+boxplot(USJudgeRatings$RTEN, horizontal = TRUE, notch =  TRUE, xlab = "Lawyer Ratings", ylim = c(0,10),col = "slategray3")
+boxplot(USJudgeRatings$RTEN, horizontal = TRUE, notch =  TRUE, xlab = "Lawyer Ratings", ylim = c(0,10),col = "slategray3",boxwex = 0.5)
+boxplot(USJudgeRatings$RTEN, horizontal = TRUE, notch =  TRUE, xlab = "Lawyer Ratings", ylim = c(0,10),col = "slategray3",boxwex = 0.5,whisklty = 1)
+boxplot(USJudgeRatings,      horizontal = TRUE, notch =  TRUE, xlab = "Lawyer Ratings", ylim = c(0,10),col = "slategray3",boxwex = 0.5,whisklty = 1, main = "Lawyers Ratings Vote Pole,America, /n 2001 to 2020")
+
+
+library(ggplot2)
+cars     <- read.csv("C:/Users/user/Desktop/Data_Visualization_with_ggplot2/Datasets/cars.csv")
+cars_merc<- read.csv("C:/Users/user/Desktop/Data_Visualization_with_ggplot2/Datasets/cars_merc.csv")
+head(cars)
+head(cars_merc)
+qplot(data=cars_merc, y=mpg, x=carname, geom="point")
+qplot(data=cars_merc, y=mpg, x=carname, geom="point", color=cyl)
+
+
+ggplot(data=cars_merc, aes(x = carname, y = mpg))
+ggplot(data=cars_merc, aes(x = carname, y = mpg)) + geom_point()
+ggplot(data=cars_merc, aes(x = carname, y = mpg)) + geom_point(aes(color = cyl))
+ggplot(data=cars_merc, aes(x = carname, y = mpg)) + geom_point(aes(color = cyl, size = 2 ))
+
+
+ggplot(data = cars_merc, aes(mpg)) 
+ggplot(data = cars_merc, aes(mpg)) + geom_area(stat = "bin", binwidth = 1)
+ggplot(data = cars_merc, aes(mpg)) + geom_area(stat = "bin", binwidth = 2)

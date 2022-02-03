8d9a701eb6366a28b28c9024202684fb3d97d890
mazed <mazedur@gmail.com>
2022-02-03 14:13:46 +0600

    ggplot2 with axis label modify

--- ggplot2_geom_axis.R
+++ ggplot2_geom_axis.R
@@ -12,7 +12,7 @@ unique(college$loan_default_rate)
 
 college <- college %>% 
   mutate(loan_default_rate = as.numeric(loan_default_rate))
-  
+
 ggplot(data = college)
 
 ggplot(data = college) + 
@@ -29,3 +29,17 @@ ggplot(data = college) +
   xlab("Region") +
   ylim(0,500)
   
+library(tidyverse)
+library(ggplot2)
+college <- read.csv("college.csv")
+college <- college %>% 
+  mutate(state = as.factor(state), region = as.factor(region), highest_degree = as.factor(highest_degree), gender = as.factor(gender),loan_default_rate = as.numeric(loan_default_rate))
+
+ggplot(data = college) +
+  geom_bar(mapping = aes(x = region, fill = control)) +
+  theme(panel.background = element_blank()) +
+  theme(plot.background = element_blank()) +
+  scale_x_discrete(name = "Region") +
+  scale_y_continuous(name = "Number of Schools", limits = c(0,500)) +
+  scale_fill_manual(values = c("orange","skyblue"), guide = guide_legend(title = "Institution Types", nrow =1, label.position = "bottom", keywidth = 2.5)) +
+  theme(legend.position = "right")
